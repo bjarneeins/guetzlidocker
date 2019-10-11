ddaf4b6d625a27384f714f5c6fc4e5f02ffd5867
bjarneeins <32593181+bjarneeins@users.noreply.github.com>
2019-10-11 19:09:33 +0200

    Added script

---
+++ guetzlidocker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+path=${1%/*}
+cd $path
+
+for i in "$@"
+do
+    file=$(basename "$i")
+
+    filestrip=${file%.*}
+    guetzlifile=("$filestrip"_guetzli.jpg)
+
+    docker run --rm -it -v "$(pwd):/work" -w /work icedream/guetzli \
+    --quality 85 $file $guetzlifile
+
+done
